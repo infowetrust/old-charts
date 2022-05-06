7043b6af90445be3d4c96aca80fdf90add31932f
RJ Andrews <rj@infowetrust.com>
2022-05-06 11:34:14 -0700

    testing hash links

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ export default function App() {
     <div>
       <h1>&nbsp;CHARTS!</h1>
       <p className="tagline">Analog excellence digitized for the web.<br />By RJ Andrews and friends.</p>
-      <div className="line"></div>
+      <div id="gold" className="line"></div>
       <Gold />
       <p>This diverging bar-chart compares the ratio of gold reserves of central banks to paper currency in circulation (left) to the relation of exchange rates to par value. It appeared as figure 102 in Karl. G. Karsten's <i>Charts and Graphs</i> (1925) with the caption, "Correlation is Indicated by Mirroring." </p>
       <p>Karsten exhibited this chart in a chapter on composite bar-charts as an answer to overly-complex designs. His text explained:</p>
       <p>"In fact, it can be laid down as a general rule that both the compound and the multiple bar-charts are too elaborate and complicated. A chart is always better the simpler it is, and we should make strong efforts to simplify these charts, and if possible reduce them to simple bar-charts. It usually pays well for sacrifices we make in this way, in legibility and interest to the reader, and after all, the chart of this type is generally directed at a reader, rather than at the maker. The only one of the three which stands out as absolutely simple and clear is the relative compound bar-chart, which consists of nothing more than a series of 100% bars."</p>
       <p>Karsten's original caption attributes permission to Carl Snyder and dates the chart to March, 1922. Read Karsten's book at the <a target="_blank" rel="noopener" href="https://archive.org/details/in.ernet.dli.2015.13852/page/n159/mode/2up">Internet Archive</a>.</p>
       <p className="tagline"><a target="_blank" rel="noopener" href="https://github.com/infowetrust/old-charts/blob/main/src/components/Gold/index.tsx">CHART CODE</a></p>
-      <div className="line"></div>
+      <div id="column" className="line"></div>
       <Column />
       <p>This vertical bar chart, or <i>column chart</i>, features several flourishes which ehance its design: asymetric vertical axis labels, bolded baseline labels every five years, emphasized zero-baseline weight, a grid that only appears over data, and a series label that follows the shape of the trend.</p>
       <p>This chart was inspired by an inset graphic by Émile Cheysson, "Navigation Intérieure. II. Tonnage kilométrique," in the 1906 <i>Album du statistique graphique</i>. See the original, an overlapping column chart, in the David Rumsey <a target="_blank" rel="noopener" href="https://www.davidrumsey.com/luna/servlet/s/255jh3">Map&nbsp;Collection</a>.</p>
